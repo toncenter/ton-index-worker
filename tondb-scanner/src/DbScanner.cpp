7dc283208c8ef08caaf990b6a8adcb3b91b467a9
K-Dimentional Tree <kdimentionaltree@gmail.com>
2024-01-12 21:37:40 +0000

    WIP

--- tondb-scanner/src/DbScanner.cpp
+++ tondb-scanner/src/DbScanner.cpp
@@ -390,7 +390,13 @@ public:
 void DbScanner::start_up() {
   alarm_timestamp() = td::Timestamp::in(1.0);
 
-  db_ = td::actor::create_actor<ton::validator::RootDb>("db", td::actor::ActorId<ton::validator::ValidatorManager>(), db_root_);
+  // create opts
+  auto opts_ = ton::validator::ValidatorManagerOptions::create(
+    ton::BlockIdExt{ton::masterchainId, ton::shardIdAll, 0, ton::RootHash::zero(), ton::FileHash::zero()},
+    ton::BlockIdExt{ton::masterchainId, ton::shardIdAll, 0, ton::RootHash::zero(), ton::FileHash::zero()}
+  );
+
+  db_ = td::actor::create_actor<ton::validator::RootDb>("db", td::actor::ActorId<ton::validator::ValidatorManager>(), db_root_, std::move(opts_));
   db_caching_ = td::actor::create_actor<DbCacheWrapper>("cache_db", db_.get(), max_db_cache_size_);
 }
 
