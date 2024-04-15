63aa3eeff0e296bfc6c9727a038b3addd831ee38
K-Dimentional Tree <kdimentionaltree@gmail.com>
2024-04-15 17:53:52 +0000

    Updated scripts

--- scripts/create_indexes.sql
+++ scripts/create_indexes.sql
@@ -2,32 +2,31 @@ begin;
 
 -- blocks
 create index if not exists blocks_index_2 on blocks (gen_utime);
-create index if not exists blocks_index_3 on blocks (mc_block_workchain, mc_block_shard, mc_block_seqno);
-create index if not exists blocks_index_3a on blocks(mc_block_seqno);
+create index if not exists blocks_index_3 on blocks(mc_block_seqno);
 create index if not exists blocks_index_4 on blocks (seqno) where (workchain = '-1'::integer);
 create index if not exists blocks_index_5 on blocks (start_lt);
 
 -- transactions
 create index if not exists transactions_index_1 on transactions (block_workchain, block_shard, block_seqno);
-create index if not exists event_detector__transaction_index_1 on transactions (lt) where (trace_id IS NULL);
 create index if not exists transactions_index_2 on transactions (account, lt);
-create index if not exists transactions_index_2a on transactions (account, now);
-create index if not exists transactions_index_3 on transactions (lt, hash);
-create index if not exists transactions_index_4 on transactions (now, hash);
-create index if not exists transactions_index_5 on transactions (hash);
-create index if not exists transactions_index_6 on transactions (trace_id);
+create index if not exists transactions_index_3 on transactions (account, now);
+create index if not exists transactions_index_4 on transactions (lt, hash);
+create index if not exists transactions_index_5 on transactions (now, hash);
+create index if not exists transactions_index_6 on transactions (hash);
+create index if not exists transactions_index_7 on transactions (trace_id);
 create index if not exists transactions_index_8 on transactions (mc_block_seqno);
+create index if not exists event_detector__transaction_index_1 on transactions (lt) where (trace_id IS NULL);
 
 -- messages
--- create index if not exists messages_index_0 on messages (tx_hash, tx_lt);
 create index if not exists messages_index_1 on messages (msg_hash);
 create index if not exists messages_index_2 on messages (source, created_lt);
 create index if not exists messages_index_3 on messages (destination, created_lt);
 create index if not exists messages_index_4 on messages (created_lt);
-create index if not exists messages_index_5 on messages (body_hash, created_lt);
+create index if not exists messages_index_5 on messages (body_hash);
 
 -- account states
 create index if not exists latest_account_states_index_1 on latest_account_states (balance);
+create index if not exists latest_account_states_address_book_index on latest_account_states (account) include (account_friendly);
 
 -- jettons
 create index if not exists jetton_masters_index_2 on jetton_masters (admin_address);
