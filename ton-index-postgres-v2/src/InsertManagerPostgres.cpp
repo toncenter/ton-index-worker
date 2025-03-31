411723ffa26dce2f9588ec41bd7ac0fbd93fa85d
Chuan-Chun Wang <a2468834@gmail.com>
2025-03-31 19:40:48 +0800

    Fix the misplacement of compute phase's gas_fees and gas_used value.

--- ton-index-postgres-v2/src/InsertManagerPostgres.cpp
+++ ton-index-postgres-v2/src/InsertManagerPostgres.cpp
@@ -614,7 +614,7 @@ void InsertBatchPostgres::insert_transactions(pqxx::work &txn, bool with_copy) {
                 std::nullopt, std::nullopt};
       } else if constexpr (std::is_same_v<T, schema::TrComputePhase_vm>) {
         return {false, std::nullopt, arg.success, arg.msg_state_used, arg.account_activated, 
-                arg.gas_used, arg.gas_fees, arg.gas_limit, arg.gas_credit, 
+                arg.gas_fees, arg.gas_used, arg.gas_limit, arg.gas_credit, 
                 arg.mode, arg.exit_code, arg.exit_arg, arg.vm_steps, 
                 arg.vm_init_state_hash, arg.vm_final_state_hash};
       } else {
